f2911b451f2fbb7790503e4cf3d82da9744e48bf
Paul Easter <paul.john.easter@gmail.com>
2021-08-21 21:23:08 +1000

    Partial changes, about to calculate difference vectors between each vertex

--- src/main.rs
+++ src/main.rs
@@ -65,17 +65,63 @@ impl Coordinate {
 }
 
 
+struct CoordinateVector {
+
+    size: usize,
+    data:  Vec<Coordinate>,
+}
+
+impl CoordinateVector {
+    
+    fn new(data : Vec<Coordinate> ) -> CoordinateVector{
+        // self.data = data;
+        // self.size = self.data.len();
+        // self
+
+        CoordinateVector{
+
+            size: data.len(),
+            data : data,
+        }
+    }
+
+    fn print(self) {
+
+        println!("Length = {}",self.size);
+        for coordinate in self.data {
+            println!("( {:.3} , {:.3} , {:.3} )",coordinate.x,coordinate.y,coordinate.z)
+        }
+
+    }
+
+    fn get_size(self) -> usize {
+        self.size
+    }
+
+
+}
+
+
 fn main() {
     let x1 = Coordinate::new(0. , 0.,-1.);
     let mut x2 = Coordinate::new(1. , 0.,-0.);
     let mut x3 = Coordinate::new(0. , 1.,-0.);
     let mut x4 = Coordinate::new(0. , 0., -1.);
 
-    c.x = 4.;
-    c.y = 33.;
-    let d = a.add(b).add(c);
-    println!("{:.3} {:.3} {:.3}",d.x,d.y,d.z);
-    let x:Coordinate = a.add(b);
+    let data = vec![x1,x2,x3,x4];
+    let mut coordinates = CoordinateVector::new(data);
+    let mut max_error = 10000.0;
+
+    // coordinates.print();
+
+    // let size = coordinates.get_size();
+
+    while max_error > 0.01 {
+        for vec1 in coordinates.data.into_iter(){
+
+        }
+    }
+
 
 
 }
