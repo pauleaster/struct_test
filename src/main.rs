73521f758ef96f734806155c23e80eccf57a2aa4
Paul Easter <paul.john.easter@gmail.com>
2021-08-22 16:53:27 +1000

    About to test repositioning of the vectors

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,46 @@ impl Coordinate {
         
     }
 
+    fn mult(&self, scale: f64) -> Coordinate {
+        Coordinate {
+            x: scale * self.x ,
+            y: scale * self.y ,
+            z: scale * self.z, 
+        }
+
+    }
+
+    fn reposition(&self, pt: &Coordinate, scale:f64) -> Coordinate {
+
+        // println!("reposition");
+        // let x = &self.delta(pt);
+        // println!("x = ");
+        // x.print();
+        // let dx = x.mult(scale);
+        // println!("dx = ");
+        // dx.print();
+        // println!("self = ");
+        // self.print();
+        // let y = self.add(&dx);
+        // println!("y = ");
+        // y.print();
+        // let z = y.make_unit_vector();
+        // println!("z = ");
+        // z.print();
+
+        // let w = self.add(&self.delta(pt).mult(scale)).make_unit_vector();
+        // println!("w = ");
+        // w.print();
+
+
+        self.add(&self.delta(pt).mult(scale)).make_unit_vector()
+    }
+
+    fn make_unit_vector(&self) -> Coordinate{
+
+        self.mult(1.0 / self.mag())
+    }
+
     fn first_quadrant(self) -> Coordinate {
 
         Coordinate {
@@ -115,10 +155,6 @@ impl CoordinateVector {
 
     }
 
-    fn max_distance(&self) {
-
-
-    }
 
 }
 
@@ -128,17 +164,20 @@ fn main() {
     let x1 = Coordinate::new(0. , 0., 1.);
     let mut x2 = Coordinate::new(1. , 0.,-0.);
     let mut x3 = Coordinate::new(0. , 1.,-0.);
-    let mut x4 = Coordinate::new(0. , 0., -1.);
+    let mut x4 = Coordinate::new(0. , 1.0/ 2.0_f64.powf(0.5), -1.0/2.0_f64.powf(0.5));
 
     let data = vec![x1,x2,x3,x4];
     let mut coordinates = CoordinateVector::new(data);
     let mut max_error = 10000.0_f64;
     let mut max_magnitude = 0.0_f64;
+    let mut min_magnitude = 0.0_f64;
     let mut max_idx1 = 0_usize;
     let mut max_idx2 = 0_usize;
     let mut this_magnitude = 0.0_f64;
     let mut max_distance = Coordinate::new(0. , 0., 0.);
     let mut difference= Coordinate::new(0. , 0., 0.);
+    let proportional_change = 1e-2_f64;
+    let mut iteration_number = 0_usize;
 
     coordinates.print();
 
@@ -159,22 +198,31 @@ fn main() {
 
                 // difference = cd2.delta(cd1); 
                 // Note difference is an &Coordinate!!!
-                print!("({},{}): ",idx1, idx2 + idx1 + 1);
+                // print!("({},{}): ",idx1, idx2 + idx1 + 1);
                 
                 let (difference, this_magnitude) = coordinate_2.delta_and_mag(coordinate_1);
                 difference.print();
-                if this_magnitude >= max_magnitude{
+                if this_magnitude > max_magnitude{
                     max_magnitude = this_magnitude;
                     max_idx1 = idx1;
                     max_idx2 = idx2;
                     max_distance = difference;
                 }
+                if this_magnitude < min_magnitude{
+                    min_magnitude = this_magnitude;
+                }
+                max_error = max_magnitude - min_magnitude
             }
         }
-        println!("\n\n\n**********Final result**********\n");
-        print!("({},{}): ",max_idx1,max_idx2);
+        println!("Iteration {}",iteration_number);
+        print!("({},{}): ",max_idx1,max_idx2+max_idx1+1);
         max_distance.print();
-        max_error = 1e-6;
+        let idx2 = max_idx2+max_idx1+1;
+        let idx1 = max_idx1;
+        coordinates.data[idx2] = coordinates.data[idx2].reposition(&coordinates.data[idx1], proportional_change);
+        coordinates.print();
+        iteration_number += 1;
+
 
     }
 
