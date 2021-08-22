fac607a1fa7d777494eccee968f0e07202c87cd4
Paul Easter <paul.john.easter@gmail.com>
2021-08-22 15:54:43 +1000

    Correctly finding the largest distance vector after significant fighting with the borrow checker

--- src/main.rs
+++ src/main.rs
@@ -16,39 +16,44 @@ impl Coordinate {
         }
     }
 
-    fn add(self, pt:Coordinate) -> Coordinate {
+    fn add(&self, pt:&Coordinate) -> Coordinate {
 
         Coordinate {
-            x: self.x + pt.x ,
-            y: self.y + pt.y ,
-            z: self.z + pt.z 
+            x: &self.x + &pt.x ,
+            y: &self.y + &pt.y ,
+            z: &self.z + &pt.z 
         }
         
     }
 
-    fn sub(self, pt:Coordinate) -> Coordinate {
+    fn sub(&self, pt:&Coordinate) -> Coordinate {
 
         Coordinate {
-            x: self.x - pt.x ,
-            y: self.y - pt.y ,
-            z: self.z - pt.z 
+            x: &self.x - &pt.x ,
+            y: &self.y - &pt.y ,
+            z: &self.z - &pt.z 
         }
         
     }
 
-    fn delta(self, pt:Coordinate) -> Coordinate {
+    fn delta_and_mag(&self, pt:&Coordinate) -> (Coordinate, f64) {
 
-        pt.sub(self)
+        (pt.delta(&self), pt.delta(&self).mag())
+    }
+    
+    fn delta(&self, pt:&Coordinate) -> Coordinate {
+
+        pt.sub(&self)
     }
 
-    fn mag(self) -> f64 {
+    fn mag(&self) -> f64 {
 
-        (self.x.powf(2.)  + self.y.powf(2.) + self.z.powf(2.)).sqrt()
+        (&self.x.powf(2.)  + &self.y.powf(2.) + &self.z.powf(2.)).sqrt()
     }
 
-    fn distance(self, pt:Coordinate) -> f64 {
+    fn distance(&self, pt:&Coordinate) -> f64 {
 
-        self.delta(pt).mag()
+        self.delta(&pt).mag()
         
     }
 
@@ -57,10 +62,25 @@ impl Coordinate {
         Coordinate {
             x: self.x.abs() ,
             y: self.y.abs() ,
-            z: self.z.abs() 
+            z: self.z.abs(), 
         }
         
     }
+
+    fn equal(&self, pt: &Coordinate) -> bool {
+        
+        let eps = 1e-6_f64;
+
+        self.distance(&pt) < eps
+
+    }
+
+    fn print(&self) {
+    
+        println!("( {:.3} , {:.3} , {:.3} ), |x| = {:.3}",self.x,self.y,self.z, &self.mag())
+    }
+
+
 
 }
 
@@ -85,43 +105,77 @@ impl CoordinateVector {
         }
     }
 
-    fn print(self) {
+    fn print(&self) {
 
-        println!("Length = {}",self.size);
-        for coordinate in self.data {
-            println!("( {:.3} , {:.3} , {:.3} )",coordinate.x,coordinate.y,coordinate.z)
+        println!("Length = {}",&self.size);
+        for coordinate in &self.data {
+            coordinate.print();
+            
         }
 
     }
 
-    fn get_size(self) -> usize {
-        self.size
-    }
+    fn max_distance(&self) {
 
+
+    }
 
 }
 
 
 fn main() {
-    let x1 = Coordinate::new(0. , 0.,-1.);
+
+    let x1 = Coordinate::new(0. , 0., 1.);
     let mut x2 = Coordinate::new(1. , 0.,-0.);
     let mut x3 = Coordinate::new(0. , 1.,-0.);
     let mut x4 = Coordinate::new(0. , 0., -1.);
 
     let data = vec![x1,x2,x3,x4];
     let mut coordinates = CoordinateVector::new(data);
-    let mut max_error = 10000.0;
+    let mut max_error = 10000.0_f64;
+    let mut max_magnitude = 0.0_f64;
+    let mut max_idx1 = 0_usize;
+    let mut max_idx2 = 0_usize;
+    let mut this_magnitude = 0.0_f64;
+    let mut max_distance = Coordinate::new(0. , 0., 0.);
+    let mut difference= Coordinate::new(0. , 0., 0.);
 
-    // coordinates.print();
+    coordinates.print();
 
-    // let size = coordinates.get_size();
+
+
 
     while max_error > 0.01 {
-        for vec1 in coordinates.data.into_iter(){
+        max_magnitude = 0.0;
+        max_idx1 = 0;
+        max_idx2 = 1;
 
+        for (idx1, coordinate_1) in coordinates.data[0..coordinates.size-1].iter().enumerate() {
+            // println!("idx1 = {}",idx1);
+            for (idx2, coordinate_2) in coordinates.data[idx1+1..coordinates.size].iter().enumerate(){
+                // println!("idx2 = {}",idx2 + idx1 + 1);
+                // let cd2 =  coordinate_2;
+                // let cd1 =  coordinate_1;
+
+                // difference = cd2.delta(cd1); 
+                // Note difference is an &Coordinate!!!
+                print!("({},{}): ",idx1, idx2 + idx1 + 1);
+                
+                let (difference, this_magnitude) = coordinate_2.delta_and_mag(coordinate_1);
+                difference.print();
+                if this_magnitude >= max_magnitude{
+                    max_magnitude = this_magnitude;
+                    max_idx1 = idx1;
+                    max_idx2 = idx2;
+                    max_distance = difference;
+                }
+            }
         }
+        println!("\n\n\n**********Final result**********\n");
+        print!("({},{}): ",max_idx1,max_idx2);
+        max_distance.print();
+        max_error = 1e-6;
+
     }
-
-
 
 }
